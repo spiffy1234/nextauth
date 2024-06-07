0d49c03184dee5c276fb4a3e7dc17fc801cfa2d9
Srishti <srishti26012000@gmail.com>
2024-06-06 20:08:38 -0600

    Updated token

--- src/helpers/getIdFromToken.ts
+++ src/helpers/getIdFromToken.ts
@@ -1,13 +1,15 @@
 import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 
-export function getIdFromToken() {
+export async function getIdFromToken() {
   try {
-    const token = cookies().get("token")?.value;
-    const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+    const token: any = await cookies().get("token")?.value;
+    const decodedToken: any = await jwt.verify(
+      token,
+      process.env.TOKEN_SECRET!
+    );
     return decodedToken.id;
   } catch (error: any) {
     throw new Error(error.message);
   }
 }
-
