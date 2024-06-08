7b15cb16dfbed03573df961c49cb56b23bc9437c
Srishti <srishti26012000@gmail.com>
2024-06-07 19:33:14 -0600

    Updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,19 @@
 import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
 
-export function middleware(request: Request) {
-  const path = request.nextUrl.pathname;
+export function middleware(request: NextRequest) {
+  const path = request?.nextUrl.pathname!;
   let isPublicPath = path == "/login" || path == "/signup";
 
   const token = cookies().get("token")?.value;
   // console.log(token, "....");
 
   if (isPublicPath && token) {
-    return Response.redirect(new URL("/", request.nextUrl));
+    return Response.redirect(new URL("/", request.nextUrl!));
   }
 
   if (!isPublicPath && !token) {
-    return Response.redirect(new URL("/login", request.nextUrl));
+    return Response.redirect(new URL("/login", request.nextUrl!));
   }
 }
 
